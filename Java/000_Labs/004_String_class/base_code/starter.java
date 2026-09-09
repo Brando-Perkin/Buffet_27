39207a592758273d2304399c9f62a88001f99990
Brando-Perkin <brandosequoia@icloud.com>
2026-09-09 21:45:33 +0000

    update

--- Java/000_Labs/004_String_class/base_code/starter.java
+++ Java/000_Labs/004_String_class/base_code/starter.java
@@ -8,7 +8,18 @@ import java.util.Scanner;
 class starter {
 	public static void main(String args[]) {
 		// the string "I love to learn coding remotely." will appear in
-		// the command window when you compile and run this program.
-		System.out.print("I love to learn coding remotely."); 
+		// the command window when you compile and run this program. 
+		String fred = new String("Happy birthday, to you");
+		String jose = new String("Happy birthday, dear Bob!");
+		System.out.println(fred);
+		System.out.println(fred);
+		System.out.println(jose);
+		System.out.println(fred);
+
+
+
+
+
+
 	}
 }
--- Java/000_Labs/005_int/base_code/starter.java
+++ Java/000_Labs/005_int/base_code/starter.java
@@ -9,6 +9,23 @@ class starter {
 	public static void main(String args[]) {
 		// the string "I love to learn coding remotely." will appear in
 		// the command window when you compile and run this program.
-		System.out.print("I love to learn coding remotely."); 
+		System.out.println("I love to learn coding remotely."); 
+		int joe;
+		joe = 589;
+		System.out.println(joe);
+		int tom;
+		int sue; 
+		tom = 2; 
+		sue = 3;
+		System.out.println(tom+sue);
+		System.out.println(tom-sue);
+		System.out.println(tom*sue);
+		System.out.println(tom/sue);
+		System.out.println(sue/tom);
+
+
+
+
+
 	}
 }
