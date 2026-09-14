dc71b6e3ae9875906dc81e6a3e353baa441113d0
Brando-Perkin <brandosequoia@icloud.com>
2026-09-14 21:58:48 +0000

    update

--- Java/000_Labs/005_int/base_code/starter.java
+++ Java/000_Labs/005_int/base_code/starter.java
@@ -23,6 +23,10 @@ class starter {
 		System.out.println(tom/sue);
 		System.out.println(sue/tom);
 
+		System.out.println(5 * 7 / 2 % 3 - 1);
+        System.out.println(10 % 3 - 4 * 7 + 2);
+        System.out.println((3 - 7 / 2 * 5) % 10);
+
 
 
 
--- Java/000_Labs/006_doubles/base_code/starter.java
+++ Java/000_Labs/006_doubles/base_code/starter.java
@@ -9,6 +9,19 @@ class starter {
 	public static void main(String args[]) {
 		// the string "I love to learn coding remotely." will appear in
 		// the command window when you compile and run this program.
-		System.out.print("I love to learn coding remotely."); 
+		System.out.println("I love to learn coding remotely."); 
+
+		double joe;
+		joe = 3.14; 
+		System.out.println(joe); 
+
+		double Celcius;
+		Celcius = (76 - 32) * 5.0/9;
+		System.out.println(Celcius);
+
+
+
+
+
 	}
 }
